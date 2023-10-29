1c93fff00ddf1b1989f11fa8ca83cf161ed6e498
Asamblea Departamental de Santander <asambleadepartamentaldesantander@MacBook-Pro-de-Asamblea.local>
2023-10-29 07:31:59 -0500

    save dinamic feature save js

---
+++ myapp/management/commands/populate_distribucion_testigo_puesto_mesa_v3.py
@@ -0,0 +1,66 @@
+from myapp.models import PuestoVotacion, Mesa, Registro, Candidato
+from django.contrib.auth.models import User
+
+from django.core.management.base import BaseCommand
+
+DISTRIBUCION = '''
+Macghan311;IE TEC RAFAEL GARCIA HERREROS;25
+Macghan311;IE TEC RAFAEL GARCIA HERREROS;26
+Macghan311;IE TEC RAFAEL GARCIA HERREROS;27
+Macghan311;IE TEC RAFAEL GARCIA HERREROS;28
+Macghan311;IE TEC RAFAEL GARCIA HERREROS;29
+1098675168;IE TEC RAFAEL GARCIA HERREROS;25
+1098675168;IE TEC RAFAEL GARCIA HERREROS;26
+1098675168;IE TEC RAFAEL GARCIA HERREROS;27
+1098675168;IE TEC RAFAEL GARCIA HERREROS;28
+1098675168;IE TEC RAFAEL GARCIA HERREROS;29
+'''
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+
+        if DISTRIBUCION:
+            distribuciones = DISTRIBUCION.split('\n')
+            canditatos = Candidato.objects.all()
+            for distribucion in distribuciones:
+                data = distribucion.split(';')
+                if len(data)>2:
+                    document_id = data[0]
+                    nombre = data[1]
+                    mesa = data[2]
+
+                    if not PuestoVotacion.objects.filter(
+                        nombre=nombre,
+                    ).exists():
+                        print(">>> este puesto de votacion no esta creado: '{}'".format(nombre))
+                        continue
+
+                    puesto_votacion_obj = PuestoVotacion.objects.filter(
+                        nombre=nombre,
+                    ).first()
+
+                    if not User.objects.filter(username=document_id).exists():
+                        print(">>> este usuario no esta creado: '{}'".format(document_id))
+                        continue
+
+                    user_obj = User.objects.filter(
+                        username=document_id,
+                    ).first()
+
+                    if not mesa:
+                        continue
+
+                    if not Mesa.objects.filter(puesto_votacion=puesto_votacion_obj, numero=mesa).exists():
+                        mesa_object = Mesa(puesto_votacion=puesto_votacion_obj, numero=mesa)
+                        mesa_object.save()
+
+                    else:
+                        mesa_object = Mesa.objects.filter(puesto_votacion=puesto_votacion_obj, numero=mesa).first()
+
+                    for canditato in canditatos:
+                        print(user_obj, mesa_object, canditato)
+
+                        if not Registro.objects.filter(testigo=user_obj, mesa=mesa_object, candidato=canditato).exists():
+                            registro_object = Registro(testigo=user_obj, mesa=mesa_object, candidato=canditato)
+                            registro_object.save()
